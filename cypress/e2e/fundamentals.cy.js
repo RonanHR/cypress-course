b04209ae60ae54595c23b98b8115e4d44e0b0c9d
ronanhub <ronan.vankessel@gmail.com>
2024-09-29 16:07:28 +0200

    basic clicking test

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,6 +1,15 @@
 describe('Fundamentals spec ', () => {
-  it('contains correct header text', () => {
+  beforeEach(() => {
     cy.visit('/fundamentals')
+  })
+  it('contains correct header text', () => {
     cy.get('[data-test="fundamentals-header"]').should('contain.text', 'Testing Fundamentals')
+  })
+  it('Accordion works correctly', () => {
+    cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
+    cy.contains(/Your tests will exist in a describe block/i).should('be.visible')
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
+    cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
   })
 })
